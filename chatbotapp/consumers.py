8485a1663245f1a346d94176d6d4f4160cde97b2
Neema <neemamwende009@gmail.com>
2024-09-29 22:40:07 +0300

    updates

--- chatbotapp/consumers.py
+++ chatbotapp/consumers.py
@@ -1,5 +1,3 @@
-
-
 import json
 from channels.generic.websocket import WebsocketConsumer
 
@@ -13,9 +11,7 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
-    
-        
+       
         self.send(text_data=json.dumps({
             'message': message
         }))
