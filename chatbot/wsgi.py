f64e2dd56452446e0342873836a1ab7a5321bc05
Neema <neemamwende009@gmail.com>
2024-09-29 22:41:59 +0300

    updates

--- chatbot/wsgi.py
+++ chatbot/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 load_dotenv()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frontend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot.settings')
 
 application = get_wsgi_application()
