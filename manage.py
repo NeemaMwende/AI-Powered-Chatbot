74c7ce2f3d16835acaa909d9ca5ecab8da917569
Neema <neemamwende009@gmail.com>
2024-09-29 22:23:11 +0300

    updates

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frontend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
