f555242c27663b854aa9a9b1bee15d9c50db956c
Neema <neemamwende009@gmail.com>
2024-10-11 19:21:16 +0300

    updates

--- chatbotapp/tests.py
+++ chatbotapp/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import openai
+import os
 
-# Create your tests here.
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+try:
+    openai_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            prompt = "Hello World",
+            max_tokens=150,
+            temperature=0.5
+        )
+    
+    print("Key is valid")
+    
+except openai.error.APIError as e:
+    print(f"invalid key: {e}")
--- chatbotapp/views.py
+++ chatbotapp/views.py
@@ -4,8 +4,19 @@ import os
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from dotenv import load_dotenv
+
+load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
+# openai.api_key = api_key
+
+# Check if the API key is loaded
+# api_key = os.getenv('OPENAI_API_KEY')
+# if api_key:
+#     print(f"API key loaded successfully: {api_key[:5]}...")  # Print first 5 characters for security
+# else:
+#     print("Failed to load API key")
 
 def index(request):
     return render(request, 'index.html', {})
@@ -32,7 +43,7 @@ def generate_openai_response(message):
         openai_response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "You are an AI Assistant called NeemaBot, created by Neema to act as a mental health assistant and help users talk through any issues they are going through. Any  information regarding Neema they can directly ask her Via +254 792 366 778."},
+                {"role": "system", "content": "You are an AI Assistant called NeemaBot, created by Neema to act as a mental health assistant and help users talk through any issues they are going through especially issues concerning social injustices and discrimination. Any  information regarding Neema they can directly ask her Via +254 792 366 778."},
                 {"role": "user", "content": message}
             ],
             max_tokens=150,
